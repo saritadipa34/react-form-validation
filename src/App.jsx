6e897373b2f04259b934b7854cb4440c5941b666
saritadipa34 <saritadipa34@gmail.com>
2025-04-23 08:01:24 +0530

    input fields  are controlled

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
+import { useState } from "react";
 
 const App=()=>{
+  const initialValues={name:"",email:"",password:""};
+  const [inputValues,setInputValues]=useState(initialValues);
+
+const handleChange=(e)=>{
+  const {name,value}=e.target;
+  setInputValues({...inputValues,[name]:value})
+  console.log(inputValues)
+}
+
   return(
     <div className="app">
     <form action="" >
       <h1>Login Form</h1>
       <label htmlFor="">Name:
         <br />
-      <input type="text" id="name" value="" placeholder="Username" />
+      <input type="text" name="name" value={inputValues.name} placeholder="Username" 
+      onChange={handleChange}/>
       <br />
       </label>
       <br />
       <label htmlFor=""> Email:
         <br />
-        <input type="email" id="email" value="" placeholder="Email"/>
+        <input type="email" name="email" value={inputValues.email} placeholder="Email"
+        onChange={handleChange}/>
         <br />
       </label>
       <br />
       <label htmlFor="">Password:
         <br />
-        <input type="password" id="password" value="" placeholder="Password"/>
+        <input type="password" name="password" value={inputValues.password} placeholder="Password"
+        onChange={handleChange}/>
       </label>
       <button>Submit</button>
     </form>
